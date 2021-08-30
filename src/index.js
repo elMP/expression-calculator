16a86b73c64354e824c379792ffe020c1d89b45a
kneazle <kneazle@yandex.ru>
2021-08-30 03:42:27 +0300

    feat: add solution for one pair of brackets

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ function eval() {
 
 function expressionCalculator(expr) {
   // write your solution here
-  return calculate(expr);
+  let result = calculate(expr);
+  //return result
+  /*return Math.round(result * 10000) / 10000;
+  console.log(result)
+  result = String(calculate(expr));
+  const dot = result.indexOf('.');
+  result = result.substr(0, dot + 5);
+  return +result*/
+  return (result ^ 0) === result ? result : +result.toFixed(4);
 }
 
 function calculate(expr) {
@@ -16,7 +24,20 @@ function calculate(expr) {
     throw 'ExpressionError: Brackets must be paired';
 
   for (let i = 0, nextOperand = ''; i < expr.length; i++) {
-    if (Number.isInteger(+expr[i]))
+    // if (expr[i] == ' ')
+    //  continue;
+
+    if (expr[i] == '(') {
+      //console.log(i, expr.lastIndexOf(')'))
+      let newExpr = expr.substr(i + 1, expr.lastIndexOf(')') - i - 1);
+      expr = expr.substr(0, i) + expressionCalculator(newExpr) + expr.substr(expr.lastIndexOf(')') + 1);
+      //  console.log('0 ' + expr);
+    }
+
+    //console.log(expr);
+
+
+    if (Number.isInteger(+expr[i]) || expr[i] === '.')
       nextOperand += expr[i];
     else {
       operands.push(nextOperand);
@@ -29,6 +50,7 @@ function calculate(expr) {
 
   let result = '';
   for (let i = 0; i < operations.length; i++) {
+    //console.log(operands)
     if (operations[i] === '+' || operations[i] === '-')
       continue;
 
@@ -44,7 +66,9 @@ function calculate(expr) {
     result = doOneOperation(+a, +b, operation);
   }
 
-  return (result ^ 0) === result ? result : +result.toFixed(4);
+  return result;
+  // return (result ^ 0) === result ? result : +result.toFixed(4);
+
 }
 
 function doOneOperation(first, second, operation) {
@@ -90,4 +114,6 @@ module.exports = {
 console.log(expressionCalculator('2    + 1'));
 //console.log(expressionCalculator('2/0'));
 console.log(expressionCalculator('49 * 63 / 58 * 36'));
-console.log(expressionCalculator('84 + 62 / 33 * 10 + 15'));
+console.log(expressionCalculator('20 - 57 * 12 - (  58 + 84 * 32 / 27  )'));
+console.log(expressionCalculator('100 - 60 / 38 + (  19 / 88 * 97 / 82 / 94  ) * 92'));
+
